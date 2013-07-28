24f4ac53661187a34334f0a31c4fbcc6be1c3dee
zaphod1984 <zaphod84@gmx.de>
2013-07-28 20:24:34 +0200

    examples added

--- examples/one.js
+++ examples/one.js
@@ -1,7 +1,25 @@
 var dedupe = require('../');
 
+console.log('----------- primitive types -----------');
 var a = [1,2,2,3];
-
 var b = dedupe(a);
 
-console.log( a, b );
+console.log( a );
+console.log( 'becomes' );
+console.log( b );
+
+console.log('----------- complex types types -----------');
+var aa = [{a:2},{a:1},{a:1},{a:1}];
+var bb = dedupe(aa);
+
+console.log( aa );
+console.log( 'becomes' );
+console.log( bb );
+
+console.log('----------- complex types types with custom hasher -----------');
+var aaa = [{a:2, b:1},{a:1, b:2},{a:1, b:3},{a:1, b:4}];
+var bbb = dedupe(aaa, function(value) { return value.a; });
+
+console.log( aaa );
+console.log( 'becomes' );
+console.log( bbb );
