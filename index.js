67fd326aef6be435946570f8e603b283696bb5b0
seriousManual <seriousstuff@gmx.de>
2016-03-14 08:33:29 +0100

    no semiconlons, use ES6

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-var sigmund = require('sigmund');
+var sigmund = require('sigmund')
 
 function dedupe(client, hasher) {
-    hasher = hasher || sigmund;
+    hasher = hasher || sigmund
 
-    var clone = [];
-    var lookup = {};
+    var clone = []
+    var lookup = {}
 
     for (var i = 0; i < client.length; i++) {
-        var elem = client[i];
-        var hashed = hasher(elem);
+        var elem = client[i]
+        var hashed = hasher(elem)
 
         if (!lookup[hashed]) {
-            clone.push(elem);
-            lookup[hashed] = true;
+            clone.push(elem)
+            lookup[hashed] = true
         }
     }
 
-    return clone;
+    return clone
 }
 
-module.exports = dedupe;
+module.exports = dedupe
